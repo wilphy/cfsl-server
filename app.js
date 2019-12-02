be554108735951aaa48f408ac96ec04df6a7f888
wey <149201216@qq.com>
2019-12-02 23:18:16 +0800

    参数获取

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const Koa = require("koa");
+const parser = require("koa-bodyparser");
 const InitManager = require("./core/init");
 
 const app = new Koa();
+
+app.use(parser());
 process.cwd();
 
 InitManager.initCore(app);
