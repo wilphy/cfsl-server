c2ec1564031665c4d88ee66158f847b53aec7b35
wey <149201216@qq.com>
2019-12-08 10:33:38 +0800

    校验器的使用

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,17 +1,18 @@
 const Router = require("koa-router");
 const router = new Router();
 
+const { PositiveIntegerValidator } = require("../../validators/validator");
+
 router.post("/v1/:id/classic/latest", (ctx, next) => {
   const path = ctx.params;
   const query = ctx.request.query;
   const headers = ctx.request.header;
   const body = ctx.request.body;
-  // if (!query) {
-  if (true) {
-    const error = new global.errs.ParameterException();
-    // error.requestUrl = `${ctx.method} ${ctx.path}`;
-    throw error;
-  }
+
+  const v = new PositiveIntegerValidator().validate(ctx);
+  const id = v.get("path.id");
+
+  ctx.body = "succeed";
 });
 
 module.exports = router;
