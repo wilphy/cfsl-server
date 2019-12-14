213afa729d41f75c894667813b7d0cd13c83e173
wey <149201216@qq.com>
2019-12-14 22:25:27 +0800

    sequelize的配置

--- core/db.js
+++ core/db.js
@@ -13,9 +13,22 @@ const sequelize = new Sequelize(dbName, user, password, {
   host,
   port,
   logging: true,
-  timezone: "+08:00"
+  timezone: "+08:00",
+  define: {
+    //create_time update_time delete_time
+    timestamps: true,
+    paranoid: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    deletedAt: "deleted_at",
+    underscored: true,
+  }
+});
+
+sequelize.sync({
+  force: true
 });
 
 module.exports = {
-  Sequelize
+  sequelize
 };
