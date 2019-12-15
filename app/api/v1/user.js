a05d6590fb15a66eb41689afff993e1a8727487c
wey <149201216@qq.com>
2019-12-15 18:45:31 +0800

    操作成功的全局success处理

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,20 +1,21 @@
 const Router = require("koa-router");
-const router = new Router({
-  prefix: "/v1/user"
-});
+const router = new Router({ prefix: "/v1/user" });
+
 const { RegisterIntegerValidator } = require("../../validators/validator");
 const { User } = require("../../models/user");
+const { success } = require("../../lib/helper");
 
 //注册
 router.post("/register", async ctx => {
-  const v = new RegisterIntegerValidator().validate(ctx);
+  const v = await new RegisterIntegerValidator().validate(ctx);
   const user = {
     email: v.get("body.email"),
     password: v.get("body.password2"),
     nickname: v.get("body.nickname")
   };
 
-  User.create(user);
+  await User.create(user);
+  success();
 });
 
 module.exports = router;
