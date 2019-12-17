096eb3ba57d93d3aed5cf4e28000e2986d2ae8f2
wey <149201216@qq.com>
2019-12-17 22:53:09 +0800

    验证token令牌

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -8,15 +8,7 @@ const { PositiveIntegerValidator } = require("../../validators/validator");
 const { Auth } = require("../../../middlewares/auth");
 
 router.get("/latest", new Auth().m, async (ctx, next) => {
-  // const path = ctx.params;
-  // const query = ctx.request.query;
-  // const headers = ctx.request.header;
-  // const body = ctx.request.body;
-  // require("../../models/user");
-  // const v = await new PositiveIntegerValidator().validate(ctx);
-  // const id = v.get("body.b.e.x", (parsed = false));
-
-  // ctx.body = "succeed";
+  ctx.body = ctx.auth.uid;
 });
 
 module.exports = router;
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,36 @@
 const basicAuth = require("basic-auth");
+const jwt = require("jsonwebtoken");
 
 class Auth {
   constructor() {}
 
   get m() {
     return async (ctx, next) => {
-      const token = basicAuth(ctx.req);
-      ctx.body = token;
+      const userToken = basicAuth(ctx.req);
+      let errMsg = "token不合法";
+      if (!userToken || !userToken.name) {
+        throw new global.errs.Forbidden(errMsg);
+      }
+      try {
+        var decode = jwt.verify(
+          userToken.name,
+          global.config.security.secretKey
+        );
+      } catch (error) {
+        //token不合法
+        //token过期
+        if (error.name == "TokenExpiredError") {
+          errMsg = "token已过期";
+        }
+        throw new global.errs.Forbidden(errMsg);
+      }
+
+      ctx.auth = {
+        uid: decode.uid,
+        scope: decode.scope
+      };
+
+      await next();
     };
   }
 }
