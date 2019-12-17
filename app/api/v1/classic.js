b261a9cc15b856d9693a23fd58c6a7fba75ba621
wey <149201216@qq.com>
2019-12-17 22:34:19 +0800

    Http Basic Auth传递token令牌

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,18 +1,22 @@
 const Router = require("koa-router");
-const router = new Router();
+const router = new Router({
+  prefix: "/v1/classic"
+});
 
 const { PositiveIntegerValidator } = require("../../validators/validator");
 
-router.post("/v1/:id/classic/latest", (ctx, next) => {
-  const path = ctx.params;
-  const query = ctx.request.query;
-  const headers = ctx.request.header;
-  const body = ctx.request.body;
+const { Auth } = require("../../../middlewares/auth");
 
-  const v = new PositiveIntegerValidator().validate(ctx);
-  const id = v.get("path.id");
+router.get("/latest", new Auth().m, async (ctx, next) => {
+  // const path = ctx.params;
+  // const query = ctx.request.query;
+  // const headers = ctx.request.header;
+  // const body = ctx.request.body;
+  // require("../../models/user");
+  // const v = await new PositiveIntegerValidator().validate(ctx);
+  // const id = v.get("body.b.e.x", (parsed = false));
 
-  ctx.body = "succeed";
+  // ctx.body = "succeed";
 });
 
 module.exports = router;
---
+++ middlewares/auth.js
@@ -0,0 +1,14 @@
+const basicAuth = require("basic-auth");
+
+class Auth {
+  constructor() {}
+
+  get m() {
+    return async (ctx, next) => {
+      const token = basicAuth(ctx.req);
+      ctx.body = token;
+    };
+  }
+}
+
+module.exports = { Auth };
